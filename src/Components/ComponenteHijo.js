e7ee66bc2a71e6d102eebe87a85f3e4257221d75
Jeni077 <105757830+Jeni077@users.noreply.github.com>
2023-09-26 06:27:22 -0300

    Prueba 1

--- src/Components/ComponenteHijo.js
+++ src/Components/ComponenteHijo.js
@@ -3,7 +3,14 @@ import React from 'react'
 export const ComponenteHijo = () => {
   return (
     <div>ComponenteHijo
-        
+        Motivación, como se siente la persona 
+Cuando se tiene un ritmo acelerado hace falta tomar por lo menos
+ unos minutos para salir de la rutina 
+incluso AVECES SE PUEDEN TENER dolores musculares 
+Sobrecargas excesivas que generan principalmente de la mano de toda
+ las que mencione   un desgaste emocional
+ 
+  POR LO QUE INFLUYE EN NUESTRA SALUD MENTAL
     </div>
   )
 }
